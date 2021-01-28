47bf4aca8af5f284603dbc4eb458d780d3234a64
MBerthold <MBerthold@NB-FC-215335>
2021-01-28 14:14:52 +0100

    new version

--- src/Config/ConnectorVersion.cs
+++ src/Config/ConnectorVersion.cs
@@ -30,7 +30,7 @@
         public static string GetMainVersion()
         {
             //Get package versions from json file (msbuild)
-            return "5.0.0";
+            return "5.1.0";
         }
     }
 }
