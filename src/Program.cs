1fe00a37e72f14aaea5374b922c6e9da5ee13a2a
MBerthold <MBerthold@NB-FC-215335>
2021-04-15 13:15:25 +0200

    change service name

--- src/Program.cs
+++ src/Program.cs
@@ -36,7 +36,7 @@
 
                 ServiceRunner<ConnectorService>.Run(config =>
                 {
-                    config.SetDisplayName("AGR Connector for Qlik");
+                    config.SetDisplayName("AGR Connector Service for Qlik");
                     config.SetDescription("AnalyticsGate Reporting Service");
                     var name = config.GetDefaultName();
                     config.Service(serviceConfig =>
